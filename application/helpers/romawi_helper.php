ab2fbfb4be837ce75f6724e7a3d163bfa6369e44
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2020-11-23 07:30:24 +0700

    Fixed romawi helper

--- application/helpers/romawi_helper.php
+++ application/helpers/romawi_helper.php
@@ -3,27 +3,15 @@ function Romawi($angka)
 {
     $hsl = "";
     if ($angka < 1 || $angka > 5000) { 
-        // Statement di atas buat nentuin angka ngga boleh dibawah 1 atau di atas 5000
         $hsl = "Batas Angka 1 s/d 5000";
     } else {
         while ($angka >= 1000) {
-            // While itu termasuk kedalam statement perulangan
-            // Jadi misal variable angka lebih dari sama dengan 1000
-            // Kondisi ini akan di jalankan
             $hsl .= "M"; 
-            // jadi pas di jalanin , kondisi ini akan menambahkan M ke dalam
-            // Varible hsl
             $angka -= 1000;
-            // Lalu setelah itu varible angka di kurangi 1000 ,
-            // Kenapa di kurangi
-            // Karena statment ini mengambil 1000 untuk di konversi menjadi M
         }
     }
 
-
     if ($angka >= 500) {
-        // statement di atas akan bernilai true / benar
-        // Jika var angka lebih dari sama dengan 500
         if ($angka > 500) {
             if ($angka >= 900) {
                 $hsl .= "CM";
@@ -78,7 +66,6 @@ function Romawi($angka)
             $angka -= 1;
         }
     }
-
     return ($hsl);
 }
 ?>
