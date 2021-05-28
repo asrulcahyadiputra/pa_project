cbd7aa0d94b9859e3cd0f8afbf5240f99b70bf0a
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2021-05-28 21:15:40 +0800

    Create Model Realization

---
+++ application/models/transactions/M_realization.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_realization extends CI_Model
+{
+
+    public function all()
+    {
+        $this->db->select('a.trans_id,a.t_project_id,a.total,a.status,b.t_project_name,c.project_name,c.project_due_date,d.client_name')
+            ->from('transactions as a')
+            ->join('type_of_project as b', 'a.t_project_id=b.t_project_id')
+            ->join('project as c', 'c.trans_id=a.trans_id')
+            ->join('clients as d', 'd.client_id=a.client_id')
+            ->where('a.trans_type', 'contract');
+
+        return $this->db->get()->result_array();
+    }
+}
+
+/* End of file M_realization.php */
